c23428a30729b1eaff19d07e2ae97d0fa7be79da
fasiullah14 <61881352+fasiullah14@users.noreply.github.com>
2026-01-25 18:11:08 +0500

    @fasi changed in service page
    @fasi changed css to tailwind aed chart section and fade-up animation

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,7 @@
 import ServiceCard from "./ServiceCard";
+import ScrollReveal from "./ScrollReveal";
+import ProgressBar from "./ProgressBar";
+import DonutChart from "./DOnutChart";
 import "./Services.css";
 
 const services = [
@@ -53,14 +56,29 @@ const services = [
 
 export default function Services() {
   return (
-    <section className="services">
-      <h2 className="services-title">Our Services</h2>
+    <section className="px-6 sm:px-10 pt-28 pb-16 text-center">
+  <h2 className="text-4xl font-bold mb-12">Our Services</h2>
 
-      <div className="services-grid">
-        {services.map((service, index) => (
-          <ServiceCard key={index} {...service} />
-        ))}
-      </div>
-    </section>
+  <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-10">
+    {services.map((service, index) => (
+      <ScrollReveal key={index} delay={index * 120}>
+        <ServiceCard {...service} />
+      </ScrollReveal>
+    ))}
+  </div>
+  {/* <div className="mt-16 space-y-6 flex flex-col items-center">
+  <ProgressBar label="Brand Identity Projects" value={90} />
+  <ProgressBar label="UI / UX Design" value={85} />
+  <ProgressBar label="Marketing Campaigns" value={75} />
+</div> */}
+<div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-16 text-blue-600">
+  <DonutChart value={90} label="Brand Identity" />
+  <DonutChart value={85} label="UI / UX Design" />
+  <DonutChart value={75} label="Marketing Design" />
+</div>
+
+
+
+</section>
   );
 }
